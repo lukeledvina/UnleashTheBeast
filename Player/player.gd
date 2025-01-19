adf41ccb569e50c9257074bb854efaab5c32c118
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-19 17:51:05 -0500

    Add player scene

---
+++ Player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+const speed: int = 200
+
+func _physics_process(delta):
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	move_and_slide()
+
+func _process(delta):
+	pass
