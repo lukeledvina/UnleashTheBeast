6d22ab7130de444df170b53a08ffcec048876660
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-19 18:01:22 -0500

    Update TODO

--- Player/player.gd
+++ Player/player.gd
@@ -4,7 +4,7 @@ const speed: int = 200
 
 func _physics_process(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	velocity.x = input_direction * speed
 	move_and_slide()
 
 func _process(delta):
