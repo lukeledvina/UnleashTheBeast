f835d956739eb40708e083915b601cceb1a260e6
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-20 16:10:29 -0500

    Add initial breakable

--- Breakable/breakable.gd
+++ Breakable/breakable.gd
@@ -4,6 +4,7 @@ var score_value: int = 100
 
 signal broken(score_value)
 
-func _on_area_entered(_area):
-	emit_signal("broken", score_value)
-	queue_free()
+
+func _on_body_entered(body):
+	broken.emit(score_value)
+	self.queue_free()
