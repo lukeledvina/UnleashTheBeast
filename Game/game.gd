e6b8e270a2f92e29b5d8bbac7c3683d377cc6110
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-20 18:34:00 -0500

    Start of work on score counter

--- Game/game.gd
+++ Game/game.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+var total_score: int = 0
+
+
+func _ready():
+	var breakables = $Level/Breakables.get_children()
+	for breakable in breakables:
+		breakable.connect("broken", _on_breakable_broken)
+		print("sup")
+
+
+func _on_breakable_broken(score):
+	total_score += score
+	print(total_score)
