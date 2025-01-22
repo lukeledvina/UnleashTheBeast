279ae5bbbd497513db4e74df64ccb2b30e579d81
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-22 09:30:21 -0500

    Combo level now affects speed

--- Game/game.gd
+++ Game/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var ui = $UI
 @onready var score_manager = $ScoreManager
+@onready var player = $Player
 var total_score: int = 0
 @onready var breakables = $Level/BreakableContainer.get_children()
 
@@ -17,6 +18,8 @@ func _on_breakable_broken(score):
 	combo_manager.increase_combo(score)
 	ui.update_score_label(score_manager.total_score)
 	ui.update_combo_labels(combo_manager.combo_progress, combo_manager.combo_level)
+	player.change_speed(combo_manager.combo_level)
 
 func _on_combo_timeout():
 	ui.update_combo_labels(combo_manager.combo_progress, combo_manager.combo_level)
+	player.change_speed(combo_manager.combo_level)
--- Player/player.gd
+++ Player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @onready var camera: Camera2D = $Camera2D
 @onready var sprite: Sprite2D = $Sprite2D
 
-var max_speed: int = 200
-var min_speed: int = 100
+
+var speed: int = 150
 var acceleration: float = 0.1
 var drift_speed: float = 1.5
 var turn_speed: float = 0.75
@@ -22,15 +22,28 @@ func _physics_process(_delta) -> void:
 		rotation_degrees += turn_speed * input_direction.x
 		input_direction.y = -1
 		input_direction = input_direction.normalized()
-		velocity = velocity.lerp(input_direction.rotated(deg_to_rad(rotation_degrees)) * max_speed, acceleration)
+		velocity = velocity.lerp(input_direction.rotated(deg_to_rad(rotation_degrees)) * speed, acceleration)
 	elif input_direction.y != 0:
-		velocity = velocity.lerp(input_direction.rotated(deg_to_rad(rotation_degrees)) * max_speed, acceleration)
+		velocity = velocity.lerp(input_direction.rotated(deg_to_rad(rotation_degrees)) * speed, acceleration)
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, friction)
 	move_and_slide()
 	
 
-		
+func change_speed(combo_level: int) -> void:
+	match combo_level:
+		0:
+			speed = 150
+		1:
+			speed = 200
+		2:
+			speed = 250
+		3:
+			speed = 300
+		4: 
+			speed = 350
+		5:
+			speed = 400
 
 
 func drift(direction) -> void:
