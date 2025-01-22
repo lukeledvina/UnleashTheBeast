37690b4335762dce900e11cb09daf39c145b1fba
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-22 14:33:11 -0500

    Breakables that are above current combo level now collide with player

--- Breakable/breakable.gd
+++ Breakable/breakable.gd
@@ -1,13 +1,19 @@
-extends Area2D
+extends RigidBody2D
 
 @export var score_value: int = 100
 @export var combo_value: int = 100
 @export var combo_threshold: int = 0
 
+@onready var rigidbody_collision_shape = $CollisionShape2D
+
 signal broken(score_value, combo_value)
 
+func _ready():
+	rigidbody_collision_shape.set_deferred("disabled", true)
 
-func _on_body_entered(body):
+func _on_area_2d_body_entered(body):
 	if body.combo_level >= combo_threshold:
 		broken.emit(score_value, combo_value)
 		self.queue_free()
+	else:
+		rigidbody_collision_shape.set_deferred("disabled", false)
